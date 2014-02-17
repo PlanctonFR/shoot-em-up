a3fc7bad184b58ecab1f184cdf2f9b63ecbbd938
Victor Bouvier-Deleau <victor.bouvierdeleau@gmail.com>
2014-02-17 23:01:29 +0100

    Correction on licenses

--- inc/global.h
+++ inc/global.h
@@ -4,7 +4,7 @@
     shoot-em-up is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+    any later version.
 
     shoot-em-up is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
